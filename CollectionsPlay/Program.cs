b3b8ac33654561371fc7365f3db2c77e46972369
Adelo97 <adel997.ak@gmail.com>
2024-10-14 15:00:58 +0200

    Shuffeld List

--- CollectionsPlay/Program.cs
+++ CollectionsPlay/Program.cs
@@ -4,7 +4,7 @@ using System;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Collection!");
 var _seeder = new csSeedGenerator();
-System.Console.WriteLine("Hi");
+System.Console.WriteLine("Hi Dragon");
 
 List<int> ints1 = new List<int>();
 for (int i = 0; i < 50; i++)
@@ -12,8 +12,26 @@ for (int i = 0; i < 50; i++)
     ints1.Add(_seeder.Next(0, 100));
 }
 WriteList("Random List", ints1);
+ints1.Sort();
+WriteList("Sorted List", ints1);
+
+for  (int i = 0; i < 100; i++)
+{
+    var idx1  = _seeder.Next(0, ints1.Count);
+    var idx2  = _seeder.Next(0, ints1.Count);
+
+    //Swap
+    var t  = ints1[idx1];
+    ints1[idx1] = ints1[idx2];
+    ints1[idx2] = t;
+
+}
+
+System.Console.WriteLine("Shuffeld List");
+
 
 static void WriteList(string header, List<int> ints1)
+
 {
     System.Console.WriteLine(header);
     var c = 0;
--- CollectionsPlay/obj/Debug/net8.0/CollectionsPlay.AssemblyInfo.cs
+++ CollectionsPlay/obj/Debug/net8.0/CollectionsPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectionsPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea9b882ddf2f60f45c16cbfbcfa74be095287ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77c64e4e6a8a766029c78e60e0b89fd8c442d32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectionsPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectionsPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
