a77c64e4e6a8a766029c78e60e0b89fd8c442d32
Adelo97 <adel997.ak@gmail.com>
2024-10-14 14:44:17 +0200

    WriteListCollections

--- CollectionsPlay/Program.cs
+++ CollectionsPlay/Program.cs
@@ -1,6 +1,28 @@
 ﻿using Seido.Utilities.SeedGenerator;
+using System;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Collection!");
 var _seeder = new csSeedGenerator();
 System.Console.WriteLine("Hi");
+
+List<int> ints1 = new List<int>();
+for (int i = 0; i < 50; i++)
+{
+    ints1.Add(_seeder.Next(0, 100));
+}
+WriteList("Random List", ints1);
+
+static void WriteList(string header, List<int> ints1)
+{
+    System.Console.WriteLine(header);
+    var c = 0;
+    foreach (var item in ints1)
+    {
+        System.Console.WriteLine($"{item,10}");
+        if ((c + 1) % 10 == 0)
+            System.Console.WriteLine();
+        c++;
+    }
+}
+
